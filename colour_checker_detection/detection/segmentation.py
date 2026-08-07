6934a27b7800179fa04c23da3b28d2fb6429a7bc
naveenramkumaroffice <naveen.ramkumar@tanuh.ai>
2026-08-07 14:21:04 +0530

    Update segmentation.py

--- colour_checker_detection/detection/segmentation.py
+++ colour_checker_detection/detection/segmentation.py
@@ -313,7 +313,7 @@ def segmenter_default(
     settings.update(**kwargs)
     #import matplotlib.pyplot as plt
 
-# Directly plot your high-precision array
+    # Directly plot your high-precision array
     #plt.figure(figsize=(10, 8))
     #plt.imshow(image)  # Matplotlib loves 0.0-1.0 float32 arrays natively
     #plt.axis("off")    # Remove pixel coordinate borders
@@ -323,15 +323,15 @@ def segmenter_default(
     #    print("the encodings")
     #    image = cctf_encoding(image)
     #    import matplotlib.pyplot as plt
-
-# Directly plot your high-precision array
-        """
-        plt.figure(figsize=(10, 8))
-        plt.imshow(image)  # Matplotlib loves 0.0-1.0 float32 arrays natively
-        plt.axis("off")    # Remove pixel coordinate borders
-        plt.title("these images")
-        plt.show()
-        """
+    
+    # Directly plot your high-precision array
+    """
+    plt.figure(figsize=(10, 8))
+    plt.imshow(image)  # Matplotlib loves 0.0-1.0 float32 arrays natively
+    plt.axis("off")    # Remove pixel coordinate borders
+    plt.title("these images")
+    plt.show()
+    """
     print("before the reformats images")
     image = reformat_image(image, settings.working_width, settings.interpolation_method)
 
